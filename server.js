aca792e4844ba7a55c1c37ee5023278f7e5352cb
Kevin Baez <soykevinkalen@gmail.com>
2021-05-18 04:47:23 -0300

    'heroku'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
 const passport = require('passport')
-
+const path = require('path')
 require('./config/database')
 require('./config/passport')
 const app = express()
@@ -12,6 +12,13 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname+"/client/build/index.html"))
+    })
+}
 
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT
